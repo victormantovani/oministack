9b2517104dd0b79c7f8e980d949b20e921fc814c
Mantovs <victor.mantovani@iv2.com.br>
2019-04-11 22:14:51 -0300

    ok

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,14 @@ const routes = require("express").Router();
 const multer = require("multer");
 const multerConfig = require("./config/multer");
 
-// const  routes = express.Router();
-
 const BoxController = require("./controller/BoxController");
-// const FileController = require("./controllers/FileController");
+const FileController = require("./controller/FileController");
 
-routes.get("/mantovs", (req, res) => {
-    return res.send("Hellow Mantovs1");
-})
+routes.post("/boxes", BoxController.store);
+routes.get("/boxes/:id", BoxController.show);
+routes.post("/boxes/:id/files",     
+    multer(multerConfig).single('file'),
+    FileController.store
+);
 
 module.exports = routes;
