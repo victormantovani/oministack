fb8e6ea4776dea1c9d087c4a9dce7f3b0fe32a0a
Mantovs <victor.mantovani@iv2.com.br>
2019-04-11 22:55:09 -0300

    ok

--- src/mantovs/FIleController.js
+++ src/mantovs/FIleController.js
@@ -1,26 +1,26 @@
-const Box = require("../model/Box");
-const File = require("../model/File");
+// const Box = require("../model/Box");
+// const File = require("../model/File");
 
 class FileController {
-    async store(req, res){
-        const box = await Box.findById(req.params.id)
+    // async store(req, res){
+    //     const box = await Box.findById(req.params.id)
         
-        const file = await File.create({
-            title: req.file.originalname,
-            path: req.file.key
-        });
+    //     const file = await File.create({
+    //         title: req.file.originalname,
+    //         path: req.file.key
+    //     });
 
-        box.files.push(file);
+    //     box.files.push(file);
 
-        await box.save();
+    //     await box.save();
         
-        //Criar um arquivo
-        // console.log(req.file);
+    //     //Criar um arquivo
+    //     // console.log(req.file);
 
-        req.io.sockets.in(box._id).emit('file',file);
+    //     req.io.sockets.in(box._id).emit('file',file);
 
-        return res.send(box);
-    }
+    //     return res.send(box);
+    // }
 }
 
 module.exports = new FileController();
